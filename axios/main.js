4dc4d10a6a3435e04990dccb6bfaa718d63aceb7
Skarner <skarner@Skarners-MacBook-Pro.local>
2021-03-14 03:21:21 +0800

    express search

--- axios/main.js
+++ axios/main.js
@@ -1,10 +1,35 @@
 
 
-document.querySelector(".get").onclick = function() {
-    axios.get("http://www.weather.com.cn/data/cityinfo/101010100.html")
-    .then(function(response){
-        console.log(response);
-    },function(err){
-        console.log(err);
-    })
-}
+var app = new Vue({
+    el:"#app",
+    data:{
+        number:'',
+        status:[],
+        expName:'',
+        timeTake:'',
+        logo:''
+    },
+
+    methods:{
+        getWeather:function(){
+            console.log(this.number);
+            var that = this;
+            axios.get('https://api.apishop.net/common/express/getExpressInfo?apiKey=lbSTdFs78db4fe3e8da4832161d4bdf23ac016191b60571&expressNumber='+this.number)
+                .then(function(response){
+                    console.log(response);
+                    that.status = response.data.result.list;
+                    that.expName = response.data.result.expName;
+                    that.timeTake = "用时："+response.data.result.takeTime;
+                    that.logo = response.data.result.logo;
+                },function(err){
+                    console.log(err);
+                })
+                .catch(function(err){})
+            
+        }
+    }
+
+})
+
+
+
